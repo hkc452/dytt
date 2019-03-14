279a92b3a110be48b14902830ea6b4a6599ce40e
hkc <779543729@qq.com>
2019-03-14 21:55:59 +0800

    run

--- index.js
+++ index.js
@@ -1,68 +1,74 @@
-const puppeteer = require('puppeteer')
-const fs = require('fs')
+    const puppeteer = require('puppeteer')
+    const fs = require('fs')
 
-const dytt = 'https://www.dytt8.net/'
+    const dytt = 'https://www.dytt8.net/'
 
 
-const init = async () => {
-    //{headless: false}
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(dytt);
+    const init = async () => {
+        //{headless: false}
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(dytt);
 
-    const data = await page.evaluate(async () => {
-        const res = []
-        // 中间区域
-        const co_area2s =  document.querySelectorAll('.bd3r .bd3rl .co_area2 ')
-        for (let co_area2 of co_area2s) {
-            const titleCon = co_area2.querySelector('.title_all > p >strong')
-            const moviesCons = co_area2.querySelectorAll('table tr')
-            const movies = []
-            for (let tr of moviesCons) {
-                const tds =tr.querySelectorAll('td')
-                if (tds.length < 2 ) continue
-                // toDO 拿到链接
-                const toGo = tds[0].children[1].href
-                
-                movies.push({
-                    title: tds[0].children[1].innerText,
-                    date: tds[1].textContent,
-                    toGo
+        const data = await page.evaluate(async () => {
+            const res = []
+            // 中间区域
+            const co_area2s =  document.querySelectorAll('.bd3r .bd3rl .co_area2 ')
+            for (let co_area2 of co_area2s) {
+                const titleCon = co_area2.querySelector('.title_all > p >strong')
+                const moviesCons = co_area2.querySelectorAll('table tr')
+                const movies = []
+                for (let tr of moviesCons) {
+                    const tds =tr.querySelectorAll('td')
+                    if (tds.length < 2 ) continue
+                    // toDO 拿到链接
+                    const toGo = tds[0].children[1].href
+                    
+                    movies.push({
+                        title: tds[0].children[1].innerText,
+                        date: tds[1].textContent,
+                        toGo
+                    })
+                }
+                res.push({
+                    title: titleCon.innerText,
+                    movies,
+                    length:moviesCons.length
                 })
             }
-            res.push({
-                title: titleCon.innerText,
-                movies,
-                length:moviesCons.length
-            })
-        }
-        return res
-    })
-    // 生成下载链接
-    for (let tag of data) {
-        if (!tag.length) continue
-        for (let movie of tag.movies) {
-            const toGoPage = await browser.newPage()
-            await toGoPage.goto(movie.toGo, {
-                timeout: 60000
-            })
-            const link = await toGoPage.evaluate(() => {
-                const links = document.querySelectorAll('#Zoom > span >table>tr>td>a')
-                if (links.length == 1) return links[0].innerText
-                const resLinks = []
-                links.forEach((i, index)=> {
-                    resLinks.push({
-                        index: index +1,
-                        link:i.innerText
-                    })
+            return res
+        })
+        const aStart = Date.now()
+        console.log('开始分析链接')
+        // 生成下载链接
+        for (let tag of data) {
+            if (!tag.length) continue
+            for (let movie of tag.movies) {
+                const toGoPage = await browser.newPage()
+                await toGoPage.goto(movie.toGo ,{
+                    timeout: 60000
                 })
-                return resLinks
-            })
-            movie.link = link
+                const link = await toGoPage.evaluate(() => {
+                    const links = document.querySelectorAll('#Zoom > span >table tr>td>a')
+                    if (links.length == 1) return links[0].innerText
+                    const resLinks = []
+                    links.forEach((i, index)=> {
+                        resLinks.push({
+                            index: index +1,
+                            link:i.innerText
+                        })
+                    })
+                    return resLinks
+                })
+                // toGoPage.close()
+                movie.link = link
+                break;
+            }
         }
+        console.log('分析结束，耗时：'+ (Date.now()-aStart)/1000 +'s')
+        // console.log(data)
+        fs.writeFileSync('1.json', JSON.stringify(data, null, 2))
+        await browser.close();
     }
-    fs.writeFileSync('1.json', JSON.stringify(data, null, 2))
-    await browser.close();
-}
 
-init()
+    init()
